1a315b4b909a4b7a64b038998660aed153e89bff
Your Name <you@example.com>
2024-11-07 18:22:15 +0100

    Add files for pip installable repository

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',                          
+    version='0.1.0',                           
+    description='A math quiz package',         
+    author='Egis Shega',                        
+    author_email='egis.shega@fau.de',      
+    url='https://github.com/shegaegis/DSSS_HW2.git',
+    packages=find_packages(),
+    install_requires=[],
+)
